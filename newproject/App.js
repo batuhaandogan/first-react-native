b35a39fde8d23c47ed85400d84968483e6af235b
batuhaandogan <tipala_1909@hotmail.com>
2018-11-01 21:53:33 -0400

    added the delete fucntion

--- newproject/App.js
+++ newproject/App.js
@@ -17,11 +17,27 @@ export default class App extends Component {
     });
   };
 
+
+
+
+  placeDeletedHandler = index => {
+    this.setState(prevState => {
+      return {
+        places: prevState.places.filter((place, i) => {
+          return i !== index;
+        })
+      };
+    });
+  };
+
+
   render() {
     return (
       <View style={styles.container}>
         <PlaceInput onPlaceAdded={this.placeAddedHandler} />
-        <PlaceList places={this.state.places} />
+        <PlaceList 
+        places={this.state.places} 
+        onItemDeleted={this.placeDeletedHandler} />
       </View>
     );
   }
@@ -36,3 +52,5 @@ const styles = StyleSheet.create({
     justifyContent: "flex-start"
   }
 });
+
+
