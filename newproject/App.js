d30822c2d217ec8c3c53f3f77f7c4cafb747d21a
batuhaandogan <tipala_1909@hotmail.com>
2018-11-01 22:30:22 -0400

    added FloatList

--- newproject/App.js
+++ newproject/App.js
@@ -12,7 +12,7 @@ export default class App extends Component {
   placeAddedHandler = placeName => {
     this.setState(prevState => {
       return {
-        places: prevState.places.concat(placeName)
+        places: prevState.places.concat({key: Math.random(), value: placeName})
       };
     });
   };
@@ -20,11 +20,11 @@ export default class App extends Component {
 
 
 
-  placeDeletedHandler = index => {
+  placeDeletedHandler = key => {
     this.setState(prevState => {
       return {
-        places: prevState.places.filter((place, i) => {
-          return i !== index;
+        places: prevState.places.filter(place => {
+          return place.key !== key;
         })
       };
     });
