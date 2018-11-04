0d08928a9d41f32b7e4636cee22285569f2931e2
batuhaandogan <tipala_1909@hotmail.com>
2018-11-03 23:57:08 -0400

    making sure it works

--- newproject/src/store/reducers/places.js
+++ newproject/src/store/reducers/places.js
@@ -1,3 +1,5 @@
+import {ADD_PLACE, DELETE_PLACE, SELECT_PLACE, DESELECT_PLACE} from '../actions/actionTypes'
+
 const initialState = {
     places: [],
     selectedPlace: null
@@ -5,6 +7,18 @@ const initialState = {
 
 const reducer = (state = initialState, action) => {
         switch(action.type) {
+            case ADD_PLACE:
+            return {
+                ...state,
+                places: prevState.places.concat({
+                    key: Math.random(),
+                    name: action.placeName,
+                    image: {
+                      uri:
+                        "https://c1.staticflickr.com/5/4096/4744241983_34023bf303_b.jpg"
+                    }
+                  })
+            };
             default: 
                     return state;
         }
